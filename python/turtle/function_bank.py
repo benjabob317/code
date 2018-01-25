c75dfe58c4c84907064938f374f9f27cde9fb53b
Benjamin G <benjamintheepic@gmail.com>
2018-01-25 17:36:45 -0500

    save

--- python/turtle/function_bank.py
+++ python/turtle/function_bank.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 def getslope(direction): #converts angle turned into slope
     if direction > 0 and direction <= 45:
-        slope = 0-(90/(((90-direction)-90))/2)
+        slope = (45/direction)
     elif direction > 45 and direction < 90:
-        slope = 1/((90/(90-direction))/2)
+        slope = (90-direction)/45
     elif direction > 90 and direction <= 135:
-        slope = 1/(90/(((180-direction)-90))/2)
+        slope = (90-direction)/45
     elif direction > 135 and direction < 180:
-        slope = 0-((90/(180-direction))/2)
+        slope = 0-(45/(180-direction))
     elif direction > 180 and direction <= 225:
-        slope = 0-(90/(((270-direction)-90))/2)
+        slope = 0-(45/(180-direction))
     elif direction > 225 and direction < 270:
-        slope = 1/((90/(270-direction))/2)
+        slope = (270-direction)/45
     elif direction > 270 and direction <= 315:
-        slope = 0-((90/(direction-270))/2)
+        slope = 0-(45/(direction-270))
     elif direction > 315 and direction < 360:
-        slope = 0-(1/((90/(360-direction))/2))
+        slope = 0-((360-direction)/45)
     else:
         slope = 'depends on program'
     return slope
