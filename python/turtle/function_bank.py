85e688cf4060fc0100efd42d579758cd33fea72e
Benjamin G <benjamintheepic@gmail.com>
2018-03-17 14:09:35 -0400

    save

--- python/turtle/function_bank.py
+++ python/turtle/function_bank.py
@@ -1,40 +1,24 @@
 #!/usr/bin/env python3
+import math
 def getslope(direction): #converts angle turned into slope
-    if direction > 0 and direction <= 45:
-        slope = (45/direction)
-    elif direction > 45 and direction < 90:
-        slope = (90-direction)/45
-    elif direction > 90 and direction <= 135:
-        slope = (90-direction)/45
-    elif direction > 135 and direction < 180:
-        slope = 0-(45/(180-direction))
-    elif direction > 180 and direction <= 225:
-        slope = 0-(45/(180-direction))
-    elif direction > 225 and direction < 270:
-        slope = (270-direction)/45
-    elif direction > 270 and direction <= 315:
-        slope = 0-(45/(direction-270))
-    elif direction > 315 and direction < 360:
-        slope = 0-((360-direction)/45)
-    else:
+    if direction in [0, 90, 180, 270, 360]:
         slope = 'depends on program'
+    else:
+        slope = 0-math.tan((math.pi*(direction-90))/180)
     return slope
 
 def reallengths(length, slope, direction): #pythagorean theorem along with terminal character widths, determines length needed to be drawn
+    mult = (length**2/(slope**2 + 1))
     if 0 < direction < 90:
-        mult = (length**2/(slope**2 + 1))
-        yln = abs((slope**2 * mult)**0.5)
-        xln = abs(mult**0.5)
+        yln = (slope**2 * mult)**0.5
+        xln = mult**0.5
     if 90 < direction < 180:
-        mult = (length**2/(slope**2 + 1))
-        yln = 0-(abs((slope**2 * mult)**0.5))
-        xln = abs(mult**0.5)
+        yln = 0-((slope**2 * mult)**0.5)
+        xln = mult**0.5
     if 180 < direction < 270:
-        mult = (length**2/(slope**2 + 1))
-        yln = 0-(abs((slope**2 * mult)**0.5))
-        xln = 0-(abs(mult**0.5))
+        yln = 0-((slope**2 * mult)**0.5)
+        xln = 0-(mult**0.5)
     if 270 < direction < 360:
-        mult = (length**2/(slope**2 + 1))
-        yln = abs((slope**2 * mult)**0.5)
-        xln = 0-(abs(mult**0.5))
-    return yln*0.45, xln
+        yln = (slope**2 * mult)**0.5
+        xln = 0-(mult**0.5)
+    return yln*0.5, xln
