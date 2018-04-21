7786d7d42e28604f342979881775afa8e3267989
Benjamin G <benjamintheepic@gmail.com>
2018-04-21 18:23:48 -0400

    save

--- python/projects/escapesequences/data.py
+++ python/projects/escapesequences/data.py
@@ -24,19 +24,23 @@ clear = '\x1b[0;0m'
 bold = '\x1b[1m'
 italic = '\x1b[3m'
 uline = '\x1b[4m'
+oline = '\x1b[53m'
 sblink = '\x1b[5m'
 fblink = '\x1b[6m'
 inverse = '\x1b[7m'
 hide = '\x1b[8m'
 strike = '\x1b[9m'
+frame = '\x1b[51m'
 
 unbold = '\x1b[22m'
 unitalic = '\x1b[23m'
-unline = '\x1b[24m'
+unuline = '\x1b[24m'
+unoline = '\x1b[55m'
 unblink = '\x1b[25m'
 uninverse = '\x1b[27m'
 unhide = '\x1b[28m'
 unstrike = '\x1b[29m'
+unframe = '\x1b[54m'
 
 #rgb(4, 17, 21) is the vscode terminal color, rgb(6, 21, 25) on the terminal
 
@@ -44,7 +48,9 @@ def text255(r, g, b): #rgb text colors, 0-255 for each color, only works on VSco
     return f'\033[38;2;{r};{g};{b}m'
 def background255(r, g, b): #rgb background colors, 0-255 for each color, only works on VScode and incorrectly
     return f'\033[48;2;{r};{g};{b}m'
+
 def text5(r, g, b): #rgb text colors, 0-5 for each color
     return f'\033[38;5;{16+(r*36)+(g*6)+b}m'
 def background5(r, g, b): #rgb background colors, 0-5 for each color
     return f'\033[48;5;{16+(r*36)+(g*6)+b}m'
+
